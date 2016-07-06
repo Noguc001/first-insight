eb802caa275385bbecce0934804259591a667255
Clara <noguc001@suttonlea.org>
2016-07-06 14:09:21 +0100

    add docstrings to functions

--- lib.py
+++ lib.py
@@ -2,10 +2,17 @@ import feedparser
 import requests
 
 def fetch_feed(url):
+    """
+    Get rss feed data from the url
+    """
     insights_feed = requests.get(url)
     return feedparser.parse(insights_feed.text)
 
+
 def format_post(post):
+    """
+    Display a post
+    """
     return ("Title: " + post['title'] + "\n \n" +
     "Summary:\n" +
     post['summary'])
